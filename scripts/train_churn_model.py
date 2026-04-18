08169b4b0c0d520153ae52945027d9d5952724b6
Yunus Bagriacik <bagriacikyunus94@gmail.com>
2026-04-18 13:03:05 +0300

    new feature has been added

--- scripts/train_churn_model.py
+++ scripts/train_churn_model.py
@@ -1,21 +1,24 @@
 import os
-import joblib #modeli kaydetmek için
+import joblib
 import pandas as pd
+import mlflow
+import mlflow.sklearn
 
 from sqlalchemy import create_engine
 from sklearn.compose import ColumnTransformer
 from sklearn.pipeline import Pipeline
 from sklearn.impute import SimpleImputer
-from sklearn.metrics import roc_auc_score, classification_report, confusion_matrix, f1_score
+from sklearn.metrics import roc_auc_score, classification_report, confusion_matrix, f1_score,precision_recall_curve
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import OneHotEncoder, StandardScaler
 from sklearn.ensemble import RandomForestClassifier
+from xgboost import XGBClassifier
 
 from app.core.config import settings
 
 
 def main():
-    engine = create_engine(settings.database_url) #config dosyasından gelen DB URL ile bağlantı kuruluyor.
+    engine = create_engine(settings.database_url)
 
     query = """
     SELECT
@@ -37,6 +40,26 @@ def main():
 
     df = pd.read_sql(query, engine)
 
+    # -----------------------------
+    # Feature Engineering
+    # -----------------------------
+    df["salary_pct_in_dept"] = df.groupby("department_name")["salary"].rank(pct=True)
+
+    dept_attrition = df.groupby("department_name")["attrition_flag"].mean()
+    df["dept_attrition_rate"] = df["department_name"].map(dept_attrition)
+
+    df["engagement_x_overtime"] = df["engagement_score"] * df["overtime_hours_monthly"]
+
+    department_market_risk = {
+        "Sales": 0.8,
+        "Technology": 0.4,
+        "HR": 0.5,
+        "Finance": 0.6,
+        "Operations": 0.7
+    }
+
+    df["dept_market_risk"] = df["department_name"].map(department_market_risk)
+
     X = df.drop(columns=["employee_code", "attrition_flag"])
     y = df["attrition_flag"].astype(int)
 
@@ -47,10 +70,13 @@ def main():
         "engagement_score",
         "absenteeism_rate",
         "overtime_hours_monthly",
+        "salary_pct_in_dept",
+        "dept_attrition_rate",
+        "engagement_x_overtime",
+        "dept_market_risk"
     ]
     binary_features = ["promoted_last_2y"]
 
-   #eksik varsa median ile doldur sonra scale et
     numeric_transformer = Pipeline(
         steps=[
             ("imputer", SimpleImputer(strategy="median")),
@@ -58,8 +84,6 @@ def main():
         ]
     )
 
-    #eksik kategorileri most_frequent ile doldur, metinleri makinenin anlayacağı sayısal vektöre çevir
-    #handle_unknown="ignore" çok önemli: API’ye yeni, daha önce görülmemiş kategori gelirse sistem patlamasın.
     categorical_transformer = Pipeline(
         steps=[
             ("imputer", SimpleImputer(strategy="most_frequent")),
@@ -67,7 +91,6 @@ def main():
         ]
     )
 
-    # Bu yapı tüm feature’lara doğru işlemi uygular.
     preprocessor = ColumnTransformer(
         transformers=[
             ("num", numeric_transformer, numeric_features),
@@ -76,15 +99,17 @@ def main():
         ]
     )
 
-    # Ham veriyi al → preprocessing yap → model uygula ve bunu tek nesne halinde sakla
     model = Pipeline(
         steps=[
             ("preprocessor", preprocessor),
-            ("classifier", RandomForestClassifier(
-                n_estimators=200,
-                max_depth=8,
+            ("classifier", XGBClassifier(
+                n_estimators=400,
+                max_depth=5,
+                learning_rate=0.05,
+                subsample=0.8,
+                colsample_bytree=0.8,
                 random_state=42,
-                class_weight="balanced"
+                eval_metric="logloss"
             )),
         ]
     )
@@ -93,25 +118,53 @@ def main():
         X, y, test_size=0.2, random_state=42, stratify=y
     )
 
-    model.fit(X_train, y_train)
 
-    y_pred = model.predict(X_test)
-    y_proba = model.predict_proba(X_test)[:, 1]
+    mlflow.set_experiment("churn_prediction")
 
-    auc = roc_auc_score(y_test, y_proba)
-    f1 = f1_score(y_test, y_pred)
+    with mlflow.start_run():
+        model.fit(X_train, y_train)
 
-    print(f"ROC AUC: {auc:.4f}")
-    print(f"F1 Score: {f1:.4f}")
-    print("\nConfusion Matrix:")
-    print(confusion_matrix(y_test, y_pred))
-    print("\nClassification Report:")
-    print(classification_report(y_test, y_pred))
+        y_proba = model.predict_proba(X_test)[:, 1]
 
-    # Eğitilmiş model dosyaya dönüştü.Artık bu model notebook’a bağlı değil.Yani servise bağlanabilir.
-    os.makedirs("artifacts", exist_ok=True)
-    joblib.dump(model, "artifacts/churn_model.joblib")
-    print("\nModel saved to artifacts/churn_model.joblib")
+        precisions, recalls, thresholds = precision_recall_curve(y_test, y_proba)
+        f1_scores = 2 * (precisions[:-1] * recalls[:-1]) / (precisions[:-1] + recalls[:-1] + 1e-9)
+        best_idx = f1_scores.argmax()
+        best_threshold = thresholds[best_idx]
+
+        print("Best threshold:", best_threshold)
+
+        y_pred = (y_proba >= best_threshold).astype(int)
+
+        auc = roc_auc_score(y_test, y_proba)
+        f1 = f1_score(y_test, y_pred)
+
+        print(f"ROC AUC: {auc:.4f}")
+        print(f"F1 Score: {f1:.4f}")
+        print("\nConfusion Matrix:")
+        print(confusion_matrix(y_test, y_pred))
+        print("\nClassification Report:")
+        print(classification_report(y_test, y_pred))
+
+        os.makedirs("artifacts", exist_ok=True)
+        joblib.dump(model, "artifacts/churn_model.joblib")
+        print("\nModel saved to artifacts/churn_model.joblib")
+
+        mlflow.log_param("model_type", "xgboost_v1")
+        mlflow.log_param("n_estimators", 400)
+        mlflow.log_param("max_depth", 5)
+        mlflow.log_param("learning_rate", 0.05)
+        mlflow.log_param("subsample", 0.8)
+        mlflow.log_param("colsample_bytree", 0.8)
+        mlflow.log_param(
+            "feature_set",
+            "baseline + salary_pct_in_dept + dept_attrition_rate + engagement_x_overtime"
+        )
+
+        mlflow.log_metric("roc_auc", auc)
+        mlflow.log_metric("f1_score", f1)
+        mlflow.log_metric("best_threshold", float(best_threshold))
+
+        mlflow.sklearn.log_model(model, "model")
 
 
 if __name__ == "__main__":
